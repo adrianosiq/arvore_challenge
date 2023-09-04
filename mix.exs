bff8a837fea95b245c5468d981a5441ac5d18a89
Adriano Siqueira <adriano@kenzie.com.br>
2023-09-04 17:10:30 -0300

    add absinthe and absinthe_plug

--- mix.exs
+++ mix.exs
@@ -32,6 +32,8 @@ defmodule ArvoreChallenge.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:absinthe, "~> 1.7"},
+      {:absinthe_plug, "~> 1.5"},
       {:phoenix, "~> 1.7.7"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.10"},
