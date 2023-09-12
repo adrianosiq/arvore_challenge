c1314d6e4db3d5a03fb9d80bc0415c4a2761cabd
Adriano Siqueira <adriano@kenzie.com.br>
2023-09-11 22:19:06 -0300

    add resolver authorization

---
+++ lib/arvore_challenge_web/resolvers/partners/authorization.ex
@@ -0,0 +1,27 @@
+defmodule ArvoreChallengeWeb.Resolvers.Partners.Authorization do
+  @moduledoc false
+
+  alias Absinthe.Resolution
+  alias ArvoreChallenge.Authorizations
+  alias ArvoreChallenge.Partners
+  alias ArvoreChallenge.Partners.Entity
+
+  @spec auhorization(map(), Resolution.t()) :: {:ok, map()} | {:error, atom()}
+  def auhorization(%{access_key: access_key, secret_access_key: secret_access_key}, _resolution) do
+    with {:ok, %Entity{} = entity} <-
+           Partners.get_entity_by_access_key_and_secret_access_key(access_key, secret_access_key),
+         {:ok, access_token, claims} <- Authorizations.encode_and_sign(entity) do
+      {:ok, build_response(access_token, claims)}
+    else
+      _ -> {:error, :unauthorized}
+    end
+  end
+
+  defp build_response(access_token, %{"exp" => exp, "iat" => iat, "typ" => typ}) do
+    %{
+      access_token: access_token,
+      expires_in: exp - iat,
+      token_type: typ
+    }
+  end
+end
---
+++ test/arvore_challenge_web/resolvers/partners/authorization_test.exs
@@ -0,0 +1,25 @@
+defmodule ArvoreChallengeWeb.Resolvers.Partners.AuthorizationTest do
+  use ArvoreChallenge.DataCase
+
+  alias ArvoreChallenge.Authorizations.Guardian
+  alias ArvoreChallengeWeb.Resolvers.Partners.Authorization
+
+  describe "auhorization/2" do
+    test "returns an access token" do
+      entity = insert!(:entity)
+      attrs = %{access_key: entity.access_key, secret_access_key: entity.secret_access_key}
+
+      assert {:ok, response} = Authorization.auhorization(attrs, %Absinthe.Resolution{})
+      assert {:ok, _claims} = Guardian.decode_and_verify(response.access_token)
+      assert response.expires_in == 86_400
+      assert response.token_type == "Bearer"
+    end
+
+    test "returns an error when not authorizer" do
+      assert Authorization.auhorization(
+               %{access_key: "XYZ", secret_access_key: "ABCDEF"},
+               %Absinthe.Resolution{}
+             ) == {:error, :unauthorized}
+    end
+  end
+end
